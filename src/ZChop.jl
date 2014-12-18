937aeb0204f743e7321480e0f2cac5f412ccdeac
John Lapeyre <john.lapeyre@gmail.com>
2014-12-18 19:36:28 +0100

    try different format

--- src/ZChop.jl
+++ src/ZChop.jl
@@ -6,11 +6,7 @@ const zchopeps = 1e-14
 
 zchop{T<:Real}(x::T, eps=zchopeps) = abs(x) > convert(T,eps) ? x : zero(T)
 zchop{T<:Complex}(x::T, eps=zchopeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
-function zchop(a::AbstractArray, eps=zchopeps)    
-    b = similar(a)
-    for i in 1:length(a)  b[i] = zchop(a[i],eps) end
-    b
-end
+zchop(a::AbstractArray, eps=zchopeps) = (b = similar(a); for i in 1:length(a)  b[i] = zchop(a[i],eps) end ; b)
 zchop(x,eps=zchopeps) = map((x)->zchop(x,eps),x)
 
 end # module ZChop
