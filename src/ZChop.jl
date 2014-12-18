cc636b1d8154877dd9db242797b3cb0a6c4e0fc0
John Lapeyre <john.lapeyre@gmail.com>
2014-12-18 21:11:00 +0100

    change identifier

--- src/ZChop.jl
+++ src/ZChop.jl
@@ -2,10 +2,10 @@ module ZChop
 
 export zchop
 
-const zchopeps = 1e-14
-zchop{T<:Real}(x::T, eps=zchopeps) = abs(x) > convert(T,eps) ? x : zero(T)
-zchop{T<:Complex}(x::T, eps=zchopeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
-zchop(a::AbstractArray, eps=zchopeps) = (b = similar(a); for i in 1:length(a)  b[i] = zchop(a[i],eps) end ; b)
-zchop(x,eps=zchopeps) = map((x)->zchop(x,eps),x)
+const zeps = 1e-14
+zchop{T<:Real}(x::T, eps=zeps) = abs(x) > convert(T,eps) ? x : zero(T)
+zchop{T<:Complex}(x::T, eps=zeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
+zchop(a::AbstractArray, eps=zeps) = (b = similar(a); for i in 1:length(a)  b[i] = zchop(a[i],eps) end ; b)
+zchop(x,eps=zeps) = map((x)->zchop(x,eps),x)
 
 end # module ZChop
