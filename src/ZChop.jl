15296e86a2f8bb49edd71233e0c5ae79f4fda618
John Lapeyre <john.lapeyre@gmail.com>
2014-12-18 19:09:57 +0100

    format

--- src/ZChop.jl
+++ src/ZChop.jl
@@ -8,7 +8,7 @@ zchop{T<:Complex}(x::T, eps=zchopeps) = complex(zchop(real(x),eps),zchop(imag(x)
 
 function zchop(a::AbstractArray, eps=zchopeps)    
     b = similar(a)
-     for i in 1:length(a)  b[i] = zchop(a[i],eps) end
+    for i in 1:length(a)  b[i] = zchop(a[i],eps) end
     b
 end
 
