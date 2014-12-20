d6fbab3d033b4195b28223c2e3c51bae4fc84f14
John Lapeyre <john.lapeyre@gmail.com>
2014-12-20 10:48:39 +0100

    factor

--- src/ZChop.jl
+++ src/ZChop.jl
@@ -6,9 +6,8 @@ const zeps = 1e-14
 zchop{T<:Real}(x::T, eps=zeps) = abs(x) > convert(T,eps) ? x : zero(T)
 zchop{T<:Integer}(x::T, eps=zeps) = abs(x) > eps ? x : zero(T)
 zchop{T<:Complex}(x::T, eps=zeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
-zchop(a::AbstractArray, eps=zeps) = (b = similar(a); for i in 1:length(a)  b[i] = zchop(a[i],eps) end ; b)
-zchop(x::String,eps=zeps) = x
-zchop(x::Char,eps=zeps) = x
+zchop(a::AbstractArray, eps=zeps) = (b = similar(a); for i in 1:length(a) b[i] = zchop(a[i],eps) end ; b)
+zchop(x::Union(String,Char),eps=zeps) = x
 zchop(x::MathConst,eps=zeps) = zchop(float(x),eps)
 zchop(x::Expr,eps=zeps) = Expr(x.head,zchop(x.args)...)
 zchop(x,eps=zeps) =  applicable(start,x) ? map((x)->zchop(x,eps),x) : x
