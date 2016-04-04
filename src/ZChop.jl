f93c4bdc53bfea1fb886939e7f276cb2fbc97d1a
John Lapeyre <lapeyre@ribot>
2016-04-04 02:20:57 +0200

    * remove cruft * remove redundant definition.
     On branch master
     Changes to be committed:
    	modified:   src/ZChop.jl

--- src/ZChop.jl
+++ src/ZChop.jl
@@ -1,28 +1,22 @@
+# Faster to not load precompiled
+#VERSION >= v"0.4.0-dev+6521" && __precompile__()
+
 module ZChop
 using Compat
 export zchop, zchop!
 
 const zeps = 1e-14
 
- zchop{T<:Real}(x::T, eps=zeps) = abs(x) > convert(T,eps) ? x : zero(T)
- zchop{T<:Integer}(x::T, eps=zeps) = abs(x) > eps ? x : zero(T)
- zchop{T<:Complex}(x::T, eps=zeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
- zchop(a::AbstractArray, eps=zeps) =
-        (b = similar(a); for i in 1:length(a) b[i] = zchop(a[i],eps) end ; b)
- zchop!(a::AbstractArray, eps=zeps) = (for i in 1:length(a) a[i] = zchop(a[i],eps) end ; a)
- zchop(x::Union{AbstractString,Char},eps=zeps) = x
- zchop(x::Irrational,eps=zeps) = zchop(float(x),eps)
-
-# @noinline zchop{T<:Real}(x::T, eps=zeps) = abs(x) > convert(T,eps) ? x : zero(T)
-# @inline zchop{T<:Integer}(x::T, eps=zeps) = abs(x) > eps ? x : zero(T)
-# @inline zchop{T<:Complex}(x::T, eps=zeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
-# @inline zchop(a::AbstractArray, eps=zeps) =
-#         (b = similar(a); for i in 1:length(a) b[i] = zchop(a[i],eps) end ; b)
-# @inline zchop!(a::AbstractArray, eps=zeps) = (for i in 1:length(a) a[i] = zchop(a[i],eps) end ; a)
-# @inline zchop(x::Union{String,Char},eps=zeps) = x
-# @inline zchop(x::Irrational,eps=zeps) = zchop(float(x),eps)
+zchop{T<:Real}(x::T, eps=zeps) = abs(x) > convert(T,eps) ? x : zero(T)
+zchop{T<:Integer}(x::T, eps=zeps) = abs(x) > eps ? x : zero(T)
+zchop{T<:Complex}(x::T, eps=zeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
+zchop(a::AbstractArray, eps=zeps) =
+    (b = similar(a); for i in 1:length(a) b[i] = zchop(a[i],eps) end ; b)
+zchop!(a::AbstractArray, eps=zeps) = (for i in 1:length(a) a[i] = zchop(a[i],eps) end ; a)
+zchop(x::Union{AbstractString,Char},eps=zeps) = x
+zchop(x::Irrational,eps=zeps) = zchop(float(x),eps)
 zchop(x::Expr,eps=zeps) = Expr(x.head,zchop(x.args)...)
-zchop(x,eps=zeps) =  applicable(start,x) ? map((x)->zchop(x,eps),x) : x
+zchop(x,eps) =  applicable(start,x) ? map((x)->zchop(x,eps),x) : x
 zchop(x) = applicable(start,x) ? map(zchop,x) : x
 
 end # module ZChop
