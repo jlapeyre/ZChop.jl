3edd097c2839a5ff178d02b6e162d09ff399a5e0
John Lapeyre <lapeyre.math122a@gmail.com>
2018-04-01 00:03:51 +0200

    update for v0.6/7 syntax

--- src/ZChop.jl
+++ src/ZChop.jl
@@ -1,17 +1,28 @@
 # Faster to not load precompiled
-#VERSION >= v"0.4.0-dev+6521" && __precompile__()
+#  __precompile__()
 module ZChop
+
 export zchop, zchop!
 
 const zeps = 1e-14
 
-zchop{T<:Real}(x::T, eps=zeps) = abs(x) > convert(T,eps) ? x : zero(T)
-zchop{T<:Integer}(x::T, eps=zeps) = abs(x) > eps ? x : zero(T)
-zchop{T<:Complex}(x::T, eps=zeps) = complex(zchop(real(x),eps),zchop(imag(x),eps))
-zchop{T<:AbstractArray}(a::T, eps=zeps) = (b = similar(a); for i in 1:length(a) b[i] = zchop(a[i],eps) end ; b)
-zchop!{T<:AbstractArray}(a::T, eps=zeps) = (for i in 1:length(a) a[i] = zchop(a[i],eps) end ; a)
-zchop{T<:Union{AbstractString,Char}}(x::T,eps=zeps) = x
-zchop{T<:Irrational}(x::T,eps=zeps) = zchop(float(x),eps)
+zchop(x::T, eps=zeps) where {T<:Real} =
+    abs(x) > convert(T,eps) ? x : zero(T)
+
+zchop(x::T, eps=zeps) where {T<:Integer} =
+    abs(x) > eps ? x : zero(T)
+
+zchop(x::T, eps=zeps) where {T<:Complex} =
+    complex(zchop(real(x),eps),zchop(imag(x),eps))
+
+zchop(a::T, eps=zeps) where {T<:AbstractArray} =
+    (b = similar(a); @inbounds for i in 1:length(a) b[i] = zchop(a[i],eps) end ; b)
+
+zchop!(a::T, eps=zeps) where {T<:AbstractArray} =
+    (@inbounds for i in 1:length(a) a[i] = zchop(a[i],eps) end ; a)
+
+zchop(x::T,eps=zeps) where {T<:Union{AbstractString,Char}} = x
+zchop(x::T,eps=zeps) where {T<:Irrational} = zchop(float(x),eps)
 zchop(x::Expr,eps=zeps) = Expr(x.head,zchop(x.args)...)
 zchop(x,eps) =  applicable(start,x) ? map((x)->zchop(x,eps),x) : x
 zchop(x) = applicable(start,x) ? map(zchop,x) : x
