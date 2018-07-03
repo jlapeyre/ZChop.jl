7d25559e0eda128c83a38526bb18e0716f28dd57
John Lapeyre <lapeyre.math122a@gmail.com>
2018-07-03 02:23:19 +0200

    reorganize, refactor, upgrade to v0.7-beta 	modified:   .travis.yml 	modified:   LICENSE.md 	modified:   REQUIRE 	modified:   appveyor.yml 	modified:   src/ZChop.jl 	modified:   test/runtests.jl

--- src/ZChop.jl
+++ src/ZChop.jl
@@ -1,30 +1,53 @@
 # Faster to not load precompiled
 #  __precompile__()
+
+"""
+The `ZChop` module provides the functions `zchop` and `zchop!`, which replace
+numbers that are close to zero with zero. These functions act recursivley on
+collections.
+"""
 module ZChop
 
 export zchop, zchop!
 
+"""
+    zeps::Float64
+
+The default lower threshold for a number to be replaced by zero.
+"""
 const zeps = 1e-14
 
-zchop(x::T, eps=zeps) where {T<:Real} =
-    abs(x) > convert(T,eps) ? x : zero(T)
+"""
+    zchop(x::T, eps::Real = zeps)
 
-zchop(x::T, eps=zeps) where {T<:Integer} =
-    abs(x) > eps ? x : zero(T)
+Replace `x` by zero if `abs(x) < eps`. `zchop` acts recursively on
+mappable objects. Objects that cannot be sensibly compared to a real
+number are passed unaltered.
+"""
+zchop(x::T, eps::Real = zeps) where {T<:Real} = abs(x) > eps ? x : zero(T)
+zchop(x::T, eps::Real = zeps) where {T<:Complex} =
+    complex(zchop(real(x), eps), zchop(imag(x), eps))
+# Following is not type stable
+zchop(x::Irrational, eps::Real = zeps) = zchop(float(x), eps)
+# Do not iterate over strings
+zchop(x::Union{AbstractString,AbstractChar}, eps::Real = zeps) = x
 
-zchop(x::T, eps=zeps) where {T<:Complex} =
-    complex(zchop(real(x),eps),zchop(imag(x),eps))
+"""
+    zchop!(x::T, eps::Real = zeps)
 
-zchop(a::T, eps=zeps) where {T<:AbstractArray} =
-    (b = similar(a); @inbounds for i in 1:length(a) b[i] = zchop(a[i],eps) end ; b)
+Perform `zchop` in place.
+"""
+function zchop!(a::AbstractArray, eps::Real = zeps)
+    @inbounds for i in firstindex(a):lastindex(a)
+        a[i] = zchop(a[i], eps)
+    end
+    return a
+end
 
-zchop!(a::T, eps=zeps) where {T<:AbstractArray} =
-    (@inbounds for i in 1:length(a) a[i] = zchop(a[i],eps) end ; a)
+zchop(a::AbstractArray, eps::Real = zeps) = zchop!(deepcopy(a), eps)
+zchop(x::Expr, eps::Real = zeps) = Expr(x.head, zchop(x.args)...)
 
-zchop(x::T,eps=zeps) where {T<:Union{AbstractString,Char}} = x
-zchop(x::T,eps=zeps) where {T<:Irrational} = zchop(float(x),eps)
-zchop(x::Expr,eps=zeps) = Expr(x.head,zchop(x.args)...)
-zchop(x,eps) =  applicable(start,x) ? map((x)->zchop(x,eps),x) : x
-zchop(x) = applicable(start,x) ? map(zchop,x) : x
+zchop(x::T, eps::Real) where {T} = Base.isiterable(T) ? map((x)->zchop(x, eps), x) : x
+zchop(x::T) where {T} = Base.isiterable(T) ? map(zchop, x) : x
 
 end # module ZChop
