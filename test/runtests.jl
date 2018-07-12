9c7183a39de593f72ea89e4704e6b52764b56a31
John Lapeyre <lapeyre.math122a@gmail.com>
2018-07-12 22:28:19 +0200

    add Project.toml
    use @testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,14 @@
 using ZChop
 using Test
 
-@test zchop(1.0) == 1.0
-@test zchop(1e-15) == 0.0
-@test zchop(1) == 1
-@test zchop(1, 3) == 0
+@testset "basic" begin
+    @test zchop(1.0) == 1.0
+    @test zchop(1e-15) == 0.0
+    @test zchop(1) == 1
+    @test zchop(1, 3) == 0
+end
 
-let z1, zz1, z2, zz2
+@testset "complex" begin
     z1 = complex(1, 1e-15)
     zz1 = complex(1, 0.0)
     @test zchop(z1) == zz1
@@ -23,38 +25,39 @@ let z1, zz1, z2, zz2
     @test zchop( [za za] ) == [zza zza]
 end
 
-@test zchop(BigFloat[1e-15, 1]) == BigFloat[0, 1]
-if Int != Int32 @test zchop(1//10^15) == 0//1 end
-@test zchop(BigInt(1)) == BigInt(1)
-@test zchop(BigInt(1), 3) == BigInt(0)
+@testset "big" begin
+    @test zchop(BigFloat[1e-15, 1]) == BigFloat[0, 1]
+    if Int != Int32 @test zchop(1//10^15) == 0//1 end
+    @test zchop(BigInt(1)) == BigInt(1)
+    @test zchop(BigInt(1), 3) == BigInt(0)
+end
 
-let r = r"cat"
+@testset "misc types" begin
+    r = r"cat"
     @test zchop(r) == r
+    @test zchop(Int) == Int
+    @test zchop([true, false]) == [true, false]
+    @test zchop('x') == 'x'
+    @test zchop(stdout) == stdout
+    @test zchop(Base.pi) == float(Base.pi)
+    @test zchop(Base.pi, 4) == 0.0
+    @test zchop(:a) == :a
+    @test zchop(:(1+1)) == :(1+1)
+    @test zchop(:(1+1e-16)) == :(1+0.0)
 end
-@test zchop(Int) == Int
-@test zchop([true, false]) == [true, false]
-@test zchop('x') == 'x'
-@test zchop(stdout) == stdout
-@test zchop(Base.pi) == float(Base.pi)
-@test zchop(Base.pi, 4) == 0.0
-@test zchop(:a) == :a
-@test zchop(:(1+1)) == :(1+1)
-@test zchop(:(1+1e-16)) == :(1+0.0)
 
-### Collections
-
-@test zchop([1e-16, "cat"]) == [0.0, "cat"]
-
-let m = [[1.0, 2.0, 1e-16] [3.0, 4.0, 5.0]]
+@testset "collections" begin
+    @test zchop([1e-16, "cat"]) == [0.0, "cat"]
+    m = [[1.0, 2.0, 1e-16] [3.0, 4.0, 5.0]]
     @test zchop(m) == [[1.0, 2.0, 0.0] [3.0, 4.0, 5.0]]
-end
-
-@test zchop(:( "cat" + [1e-16, 2.0] )) == :( "cat" + [0.0, 2.0] )
-
-let b = collect(1:5) * 1e-15, c = zchop(b)
+    @test zchop(:( "cat" + [1e-16, 2.0] )) == :( "cat" + [0.0, 2.0] )
+    b = collect(1:5) * 1e-15
+    c = zchop(b)
     zchop!(b)
     @test c == b
 end
-@test zchop((1e-15, 1, 1)) == (0.0, 1, 1)
 
-@test_throws MethodError zchop()
+@testset "error" begin
+    @test zchop((1e-15, 1, 1)) == (0.0, 1, 1)
+    @test_throws MethodError zchop()
+end
